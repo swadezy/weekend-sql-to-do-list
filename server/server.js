94f3a53b1ef345cfa3e019090c2774986cbea1ed
Sean Wade <smwade1115@gmail.com>
2021-01-08 15:48:46 -0600

    server up and running

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const todoRouter = require('./routes/todo-router.js');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('/server/public'));
+
+// app.use('/todo', todoRouter);
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+
+})
